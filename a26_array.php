efe0cd66879c8b44ec28e5c21d605fcb6bed8e7c
4A555076 <fz555076@gmail.com>
2022-12-13 11:15:35 +0800

    陣列的設定

---
+++ a26_array.php
@@ -0,0 +1,33 @@
+<prep>
+  <?php
+  $ar1 = array(2, 'bill', 6);  //舊的語法
+  $ar2 = [2, 'bill', 6];
+
+  # echo $ar2; // 不要直接輸出陣列
+
+  print_r($ar2);
+  // var_dump($ar2);
+
+  // 關聯式陣列
+  $ar3 = array(
+    'name' => 'David',
+    'age' => 25,
+  );
+
+  $ar4 = [
+    'name' => 'David',
+    'age' => 25,
+  ];
+  print_r($ar4);
+
+  $ar5 = [
+    'Hello',
+    'name' => 'David',
+    'age' => 25,
+    'world',
+  ];
+  print_r($ar5);
+
+
+  ?>
+</prep>
