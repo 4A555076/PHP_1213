1eafa06d13ad39236dc856cbec44b58eccd52d1f
4A555076 <fz555076@gmail.com>
2022-12-13 11:20:36 +0800

    操作陣列

--- a27_arraypush.php
+++ a27_arraypush.php
@@ -1,15 +1,17 @@
-<prep>
-  <?php
-  for ($i = 1; $i <= 42; $i++) {
-    $ar[] = $i;
-  }
-  echo implode(':', $ar);  //陣列內容中間字串串接
-  #explode()  //切割字串變成陣列
+<pre>
+<?php
+for ($i = 1; $i <= 42; $i++) {
+  $ar[] = $i; // Array push
+}
 
-  $ar2 = range(1, 30);  //取數值範圍當陣列元素
-  #$ar2 = range(1,30,2)
-  shuffle($ar2); //隨機排序
+echo implode(':', $ar); // 陣列內容中間字串串接
+# explode() // 切割字串變成陣列
+echo '<br>';
 
-  echo implode(':', $ar2);  //陣列內容中間字串串接
-  ?>
-</prep>
+$ar2 = range(1, 30); // 取數值範圍當陣列元素
+#$ar2 = range(1,30,2); // 取數值範圍當陣列元素
+shuffle($ar2); // 隨機排序
+
+echo implode(':', $ar2); // 陣列內容中間字串串接
+?>  
+</pre>
